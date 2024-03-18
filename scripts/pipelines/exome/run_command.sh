c9ef27466fa648630c2128f15fc2f8695c633a84
KevinYSM <kevinshahm11@gmail.com>
2024-03-18 05:06:39 +0000

    master script for exome pipeline

---
+++ scripts/pipelines/exome/run_command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#NB: all raw exome data (.fastq.gz) needs to be in a single folder
+raw_exome_data=""
+
+#00_collate and rename
+#01_trim_umis
+#02_rename
+#03_disambiguate
+#04_samplesheets
+#05_sarek
