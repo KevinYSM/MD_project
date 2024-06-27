edc6acf0f33d57e8b291bf33bb658d06179b34ce
KevinYSM <kevinshahm11@gmail.com>
2024-06-27 04:05:29 +0000

    removing exome_old

--- scripts/pipelines/exome_old/00_collate_and_rename/run_command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pwd=$(pwd | awk -F '/scripts/pipelines/exome/00_collate_and_rename' '{print $1}')
-
-python3 $pwd/scripts/pipelines/exome/helper_scripts/00_collate_and_rename.py $pwd/data/exome/raw/batched
--- scripts/pipelines/exome_old/02_bbsplit/README.md
+++
@@ -1,4 +0,0 @@
-This is a pipeline to trim umis and disambiguate exome sequencing data.
-This is called "pre_processing", as the output of this pipeline is then used in nf-core's exome (sarek) pipeline.
-
-  
